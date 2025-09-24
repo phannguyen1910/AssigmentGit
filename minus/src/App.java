64fcaed12468b5abb608d16e489dc0ec4d1c1223
phanhoaian1203 <phanhoaian10b3@gmail.com>
2025-09-24 23:25:49 +0700

    an add minus

---
+++ minus/src/App.java
@@ -0,0 +1,16 @@
+public class App {
+    
+    public static int minus(int a, int b) {
+        return a - b;
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        
+        
+
+        
+        System.out.println("5 - 2 = " + minus(5, 2));
+      
+    }
+}
