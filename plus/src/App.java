0cfa7d812a907ac05a79b0c847a531fd78ca05f6
phanhoaian1203 <phanhoaian10b3@gmail.com>
2025-09-24 23:29:19 +0700

    an add plus

---
+++ plus/src/App.java
@@ -0,0 +1,16 @@
+public class App {
+    
+    public static int plus(int a, int b) {
+        return a + b;
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        
+        
+
+        
+        System.out.println("5 + 2 = " + plus(5, 2));
+      
+    }
+}
