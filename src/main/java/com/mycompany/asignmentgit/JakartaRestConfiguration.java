c10e60162c86c9a31ba6707a32b778b89f4c286e
phannguyen1910 <nguynhuphannguyen2004@gmail.com>
2025-09-24 22:47:36 +0700

    remove nested folder

---
+++ src/main/java/com/mycompany/asignmentgit/JakartaRestConfiguration.java
@@ -0,0 +1,13 @@
+package com.mycompany.asignmentgit;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+/**
+ * Configures Jakarta RESTful Web Services for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JakartaRestConfiguration extends Application {
+    
+}
