cfb309fff6d11b05a9d010e4d3d7fd389f30a33c
phanhoaian1203 <phanhoaian10b3@gmail.com>
2025-09-24 23:35:54 +0700

    an add divide

---
+++ divide/src/App.java
@@ -0,0 +1,16 @@
+public class App {
+    
+    public static int divide(int a, int b) {
+        return a / b;
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        
+        
+
+        
+        System.out.println("5 / 2 = " + divide(5, 2));
+      
+    }
+}
