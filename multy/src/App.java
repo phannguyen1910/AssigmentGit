153bc07d02967c0917f9c37fc7e4b6965cc1c813
phanhoaian1203 <phanhoaian10b3@gmail.com>
2025-09-24 23:33:09 +0700

    an add multy

---
+++ multy/src/App.java
@@ -0,0 +1,16 @@
+public class App {
+    
+    public static int multy(int a, int b) {
+        return a * b;
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        
+        
+
+        
+        System.out.println("5 * 2 = " + multy(5, 2));
+      
+    }
+}
